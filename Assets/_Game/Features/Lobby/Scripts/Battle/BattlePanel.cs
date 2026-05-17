4ad154f5df3fc65f612e68090314edd8825ad6e6
SpyBurner <zombiepigman1505@gmail.com>
2026-05-18 01:17:26 +0700

    Fix: BattlePanel wiring

--- Assets/_Game/Features/Lobby/Scripts/Battle/BattlePanel.cs
+++ Assets/_Game/Features/Lobby/Scripts/Battle/BattlePanel.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -8,21 +9,43 @@ public class BattlePanel : UIPanel
     [Inject] private readonly IUIManagerSubsystem _uiManager;
 
     [SerializeField] private Button _startMatchmakingButton;
+    [SerializeField] private TMPro.TMP_Dropdown _playerCountDropdown;
+    [SerializeField] private Toggle _fillRoomWithAI;
 
     protected override void OnEnable()
     {
         base.OnEnable();
         _startMatchmakingButton?.onClick.AddListener(OnStartMatchmaking);
+        _playerCountDropdown?.onValueChanged.AddListener(OnPlayerCountChanged);
+        _fillRoomWithAI?.onValueChanged.AddListener(OnFillRoomWithAIChanged);
+
+        _battleSetup.SetPlayerCount(_playerCountDropdown.value);
     }
 
     protected override void OnDisable()
     {
         base.OnDisable();
         _startMatchmakingButton?.onClick.RemoveListener(OnStartMatchmaking);
+        _playerCountDropdown?.onValueChanged.RemoveListener(OnPlayerCountChanged);
+        _fillRoomWithAI?.onValueChanged.RemoveListener(OnFillRoomWithAIChanged);
     }
 
     private void OnStartMatchmaking()
     {
         _battleSetup.StartMatchmaking();
+    }
+
+    private void OnFillRoomWithAIChanged(bool isFillRoom)
+    {
+        _battleSetup.SetBotCount(isFillRoom ? 1 : 0);
+    }
+
+    private void OnPlayerCountChanged(int playerCnt)
+    {
+        _battleSetup.SetPlayerCount(playerCnt);
+
+        var botMode = playerCnt == 2 && _fillRoomWithAI.isOn;
+        _fillRoomWithAI.interactable = playerCnt == 2;
+        _battleSetup.SetBotCount(botMode ? 1 : 0);
     }
 }
