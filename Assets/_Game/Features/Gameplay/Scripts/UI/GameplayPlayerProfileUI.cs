cee35d46576bdef9cb743547ac256e66d037efce
SpyBurner <zombiepigman1505@gmail.com>
2026-05-20 00:17:02 +0700

    F1 Wiring done

--- Assets/_Game/Features/Gameplay/Scripts/UI/GameplayPlayerProfileUI.cs
+++ Assets/_Game/Features/Gameplay/Scripts/UI/GameplayPlayerProfileUI.cs
@@ -1,37 +1,65 @@
+using System;
+using Fusion;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 public class GameplayPlayerProfileUI : MonoBehaviour
 {
+    [Inject] private readonly IProfileSubsystem _profile;
+    [Inject] private readonly IPlayerCardZoneSubsystem _cardZone;
+
     [SerializeField] private TMP_Text _nameText;
     [SerializeField] private TMP_Text _hpText;
+    [SerializeField] private Toggle _readyToggle;
 
-    private NetworkPlayerState _state;
+    private PlayerRef _playerRef;
+    private bool _isLocal;
+    private bool _bound;
 
-    private void Awake()
+    public void Bind(PlayerRef playerRef, bool isLocal)
     {
-        foreach (var tmp in GetComponentsInChildren<TMP_Text>(true))
-        {
-            if (_hpText == null && tmp.name == "HPValueText") _hpText = tmp;
-            if (_nameText == null && tmp.name == "NameText") _nameText = tmp;
-        }
-    }
+        _playerRef = playerRef;
+        _isLocal = isLocal;
+        _bound = true;
 
-    public void Initialize(NetworkPlayerState state)
-    {
-        _state = state;
+        if (_readyToggle != null) _readyToggle.interactable = false;
         Refresh();
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        if (_state == null) return;
-        Refresh();
+        _cardZone.HPChanged += OnHPChanged;
+        _profile.UsernameChanged += OnUsernameChanged;
+    }
+
+    private void OnDisable()
+    {
+        _cardZone.HPChanged -= OnHPChanged;
+        _profile.UsernameChanged -= OnUsernameChanged;
+    }
+
+    private void OnHPChanged(PlayerRef p, int hp)
+    {
+        if (!_bound || p != _playerRef) return;
+        try { if (_hpText != null) _hpText.text = hp.ToString(); }
+        catch (Exception ex) { Debug.LogException(ex); }
+    }
+
+    private void OnUsernameChanged(string name)
+    {
+        if (!_bound || !_isLocal) return;
+        try { if (_nameText != null) _nameText.text = name; }
+        catch (Exception ex) { Debug.LogException(ex); }
     }
 
     private void Refresh()
     {
-        if (_hpText != null) _hpText.text = _state.HP.ToString();
-        if (_nameText != null) _nameText.text = _state.PlayerName.ToString();
+        if (_nameText != null)
+            _nameText.text = _isLocal ? _profile.Username : "Opponent";
+
+        if (_hpText != null)
+            _hpText.text = _cardZone.GetHP(_playerRef).ToString();
     }
 }
